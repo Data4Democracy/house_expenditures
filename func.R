b70e423e60ce42cb70814aaadb6c48fedd12641a
restrellado <ryan.estrellado@gmail.com>
2016-12-30 13:19:57 -0800

    Functions for calculating office and payee yearly totals.

---
+++ func.R
@@ -0,0 +1,19 @@
+library(dplyr)
+
+# What is the total yearly expenditure for an office during the Q3 measurement?
+by_office <- df %>%
+  group_by(OFFICE, QUARTER) %>%
+  summarise("yearly_exp" = sum(AMOUNT))
+
+officeYearly <- function(office) {
+  by_office[by_office$OFFICE == office, ]
+}
+
+# What is the total yearly expenditure paid to a payee during the Q3 measurement?
+by_payee <- df %>%
+  group_by(PAYEE, QUARTER) %>%
+  summarise("yearly_exp" = sum(AMOUNT))
+
+payeeYearly <- function(payee) {
+  by_payee[by_payee$PAYEE == payee, ]
+}
