926eabf693d25ee25fad50a04a206526f7ebdbb9
Josiah <josiah.parry@gmail.com>
2016-12-31 17:38:33 -0500

    first attempt of summary function

---
+++ exp_summary.R
@@ -0,0 +1,21 @@
+library(tidyr)
+library(purrr)
+library(dplyr)
+
+exp_summary <- function(df, summary_var) {
+  summarise_exp <- function(data_frame = NULL, ...) {
+    data_frame %>% 
+      group_by_(..., 'QUARTER') %>%
+      summarise(yearly_exp = sum(AMOUNT),
+                max_exp_cat = data_frame[which.max(data_frame[, 'AMOUNT']), "CATEGORY"],
+                max_exp = max(AMOUNT),
+                min_exp = min(AMOUNT),
+                sd_exp = sd(AMOUNT),
+                avg_exp = mean(AMOUNT)
+      )
+  }
+  
+  df %>% split(.$QUARTER) %>%
+    map(~ .x %>% summary_exp(summary_var))
+}
+
