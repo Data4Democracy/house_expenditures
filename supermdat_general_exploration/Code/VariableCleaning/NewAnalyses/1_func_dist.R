d984c4c69154a5cfde0d7d5a548951f650358585
MDA Turse <turse.mda@gmail.com>
2017-07-09 10:43:04 -0400

    A series of scripts to organize the data and clean the categorical variables.
    This commit contains 9 files that each begin with a number (e.g., 1_...). This number indicates the order in which the files should be run.
    
    1_func_dist.R  --  a function to calculate string distances
    2_func_dist_ByFrstChr.R  --  a function to calculate string distances when the text variable has too many (~ >10,000) unique entries
    3_func_TopMisSpel.R  --  a function to show mis-spellings in a text variable
    4_NB_GeneralOrgMinorClean.Rmd  --  a notebook that downloads and organizes the data, and does some light cleaning
    5_NB_CleanChrVar_Category  --  a notebook cleaning the "category" variable
    6_NB_CleanChrVar_Office  --  a notebook cleaning the "office" variable
    7_NB_CleanChrVar_Program  --  a notebook cleaning the "program" variable
    8_NB_CleanChrVar_Purpose  --  a notebook cleaning the "purpose" variable
    9_NB_CleanChrVar_Payee  --  a notebook cleaning the "payee" variable (NOTE: some portions of this notebook took 20-30 minutes to run on my laptop)

--- supermdat_general_exploration/Code/VariableCleaning/NewAnalyses/1_func_dist.R
+++ supermdat_general_exploration/Code/VariableCleaning/NewAnalyses/1_func_dist.R
@@ -15,53 +15,64 @@
 
 
 
+##  Load packages to be used
+library("tidyverse")          # data manipulation
+library("lazyeval")           # writing functions
+library("rlang")              # writing functions
+library("stringr")            # string manipulation
+library("stringdist")         # calculating string (character) distances
+
+
+
 func_dist <- function(data_, var_, method_ = "jw", ...){
   var_enquo <- enquo(var_)
   
   distinct_var <- select(data_,
                          !!var_enquo
-  ) %>% 
+                         ) %>% 
     distinct() %>%
     as.data.frame()
   
-  distinct_var[is.na(distinct_var)] <- "--"
+  distinct_var[is.na(distinct_var)
+               ] <- "--"
   
   
   distance <- stringdistmatrix(distinct_var[ , 1],
                                useNames = "strings",
                                method = method_
-  )
+                               )
   
   distance_matrix <- as.matrix(distance)
   
-  distance_matrix[upper.tri(distance_matrix, diag = TRUE)] <- NA
+  distance_matrix[upper.tri(distance_matrix, diag = TRUE)
+                  ] <- NA
   
   distance_matrix <- as.data.frame(distance_matrix) %>%
     mutate(level2 = rownames(distance_matrix)
-    )
+           )
   
   
   score <- paste0(method_, "_score")
   var_chr <- deparse(substitute(var_)
-  )
+                     )
   
   
   gather_data <- gather_(distance_matrix,
                          key_col = "level1",
                          value_col = score,
                          gather_cols = colnames(distance_matrix)[1:nrow(distinct_var)]
-  ) %>%
+                         ) %>%
     mutate(var_og = var_chr) %>%
     select(var_og,
            level1,
            level2,
            score
-    )
+           )
   
   
   filter_no_na <- filter(gather_data,
                          !is.na(gather_data[ ,4])
-  )
+                         )
   
   
   return(filter_no_na)
